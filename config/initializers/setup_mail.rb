b27df504d96aeb9e47e51a3c149d0afa26cd3e40
gitbnw <byron.weiss@gmail.com>
2016-01-08 16:14:57 +0000

    devise working

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,7 +1,7 @@
 ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
     :address        =>  'smtp.sendgrid.net',
-    :port           =>  '587',
+    :port           =>  '2525',
     :authentication =>  :plain,
     :user_name      =>  ENV["SENDGRID_USERNAME"],  
     :password       =>  ENV["SENDGRID_PASSWORD"],
